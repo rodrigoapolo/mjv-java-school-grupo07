5e626634f455319e64ab0d2beef72a9005083f51
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-06-08 15:52:31 -0300

    Adiciona verificacao se a conta existe

--- Conta.java
+++ Conta.java
@@ -10,14 +10,26 @@ public consultarSaldoAtual(){
 
 public void cancelarContaJustificativa(String justificativa){
 
-    // Justificativa default
-    if(justificativa == null || justificativa.isEmpty()){
-        justificativa = "Gostaria de cancelar por motivos pessoais.";
-    }
+    if(existeConta()){
 
-    System.out.println("Conta cancelada com sucesso!");
-    System.out.println("Justificativa de cancelamento: " + justificativa);
-     
+        System.out.println("Conta cancelada com sucesso!");
+
+        // Justificativa default
+        if (justificativa == null || justificativa.trim().isEmpty()) {
+            justificativa = "Gostaria de cancelar por motivos pessoais.";
+        }
+        
+        System.out.println("Justificativa de cancelamento: " + justificativa);
+
+    } else {
+        System.out.println("Erro");
+    }
  }
+
+// Verifica se a conta existe
+// Método simples, só para ilustrar
+public boolean existeConta(){
+    return true;
+}
 
 }
