a764e3231155d9bc9433853e17528f6f60c84e40
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-06-07 22:15:04 -0300

    Criado variavel nome e metodo cancelarContaJustficativa

--- Conta.java
+++ Conta.java
@@ -2,10 +2,22 @@ public class Conta {
 
 private Long numero;
 double saldo;
+private String nome;
 
 public consultarSaldoAtual(){
     System.out.println(saldo);
 }
 
+public void cancelarContaJustificativa(String justificativa){
+
+    // Justificativa default
+    if(justificativa == null || justificativa.isEmpty()){
+        justificativa = "Gostaria de cancelar por motivos pessoais.";
+    }
+
+    System.out.println("Conta cancelada com sucesso!");
+    System.out.println("Justificativa de cancelamento: " + justificativa);
+     
+ }
 
 }
