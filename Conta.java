22f71f88c54cafc127fd1c143f90215ca7b156db
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-06-11 17:05:01 -0300

    Remove logica metodo, melhora nomenclatura 'nome'

--- Conta.java
+++ Conta.java
@@ -2,7 +2,7 @@ public class Conta {
 
 private Long numero;
 double saldo;
-private String nome;
+private String nomeCliente;
 
 public consultarSaldoAtual(){
     System.out.println(saldo);
@@ -10,22 +10,7 @@ public consultarSaldoAtual(){
 
 
 public void cancelarContaJustificativa(String justificativa){
-
-    if(existeConta()){
-
-        System.out.println("Conta cancelada com sucesso!");
-
-        // Justificativa default
-        if (justificativa == null || justificativa.trim().isEmpty()) {
-            justificativa = "Gostaria de cancelar por motivos pessoais.";
-        }
-        
-        System.out.println("Justificativa de cancelamento: " + justificativa);
-
-    } else {
-        System.out.println("Erro");
-    }
- }
+}
 
 // Verifica se a conta existe
 // Método simples, só para ilustrar
