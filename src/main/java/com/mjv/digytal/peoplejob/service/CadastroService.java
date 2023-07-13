707176f9c7e0bab5d50929d13253d2135907b504
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-07-13 15:49:54 -0300

    Remove imports desnecessarios

--- src/main/java/com/mjv/digytal/peoplejob/service/CadastroService.java
+++ src/main/java/com/mjv/digytal/peoplejob/service/CadastroService.java
@@ -2,7 +2,6 @@ package com.mjv.digytal.peoplejob.service;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
