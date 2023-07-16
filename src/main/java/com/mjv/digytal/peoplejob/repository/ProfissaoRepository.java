ff91bedf2c3ab0ef6933333ae4c391df597a9ec5
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-07-16 18:33:33 -0300

    Remove flyway

--- src/main/java/com/mjv/digytal/peoplejob/repository/ProfissaoRepository.java
+++ src/main/java/com/mjv/digytal/peoplejob/repository/ProfissaoRepository.java
@@ -19,10 +19,10 @@ import java.util.List;
 @Repository
 public interface ProfissaoRepository extends JpaRepository<Profissao, Integer> {
 
-	@Query("SELECT MIN(c.pretencaoSalarial.pretencaoMinima) as salario, p.nome as profissao FROM Cadastro c INNER JOIN c.profissao p WHERE  p.nome = :profissao")
+	@Query("SELECT MIN(c.pretencaoSalarial.pretencaoMinima) as salario, p.nome as profissao FROM Cadastro c INNER JOIN c.profissao p WHERE LOWER(p.nome) = LOWER(:profissao)")
 	SalarioProfissaoView buscarSalarioMinimo(@Param("profissao") String profissao);
 
-	@Query("SELECT AVG(c.pretencaoSalarial.pretencaoMinima) as salario, p.nome as profissao FROM Cadastro c INNER JOIN c.profissao p WHERE  p.nome = :profissao")
+	@Query("SELECT AVG(c.pretencaoSalarial.pretencaoMinima) as salario, p.nome as profissao FROM Cadastro c INNER JOIN c.profissao p WHERE LOWER(p.nome) = LOWER(:profissao)")
 	SalarioProfissaoView buscarMediaSalarioMaximo(@Param("profissao") String profissao);
 	
 	@Query(value = "SELECT p.NOME AS profissao, COUNT(*) AS quantidade\n" +
